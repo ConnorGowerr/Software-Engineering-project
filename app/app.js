9ca438b71b269c1618243117fd4f06343cb77cd6
ConnorGowerr <connor.gower04@gmail.com>
2025-03-09 17:33:48 +0000

    serves html page

--- app/app.js
+++ app/app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express();
 const port = 8008;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) =>  {
-    res.send('In the front room, straight up coding it and by it lets just say, my server.')
+    //sends the static file (login page) once server is run to port 8008
+    res.sendFile('login.html', {root: 'public'}, (err) => {
+        if(err) {
+            console.log(err);
+        }
+    })
 });
 
 app.listen(port, () => {
