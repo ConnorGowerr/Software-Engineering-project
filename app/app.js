23696170eb665c310e1b53abf82de908a77d38f7
oliver fawkes <oliverfawkes@olivers-MacBook.local>
2025-03-17 14:53:45 +0000

    added stuff

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) =>  {
     //sends the static file (login page) once server is run to port 8008
-    res.sendFile('login.html', {root: 'public'}, (err) => {
+    res.sendFile('Achievement.htmls', {root: 'public'}, (err) => {
         if(err) {
             console.log(err);
         }
