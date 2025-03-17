70adb0e2d6e8af9e4fcf0d688af0d19363618383
vvk23tmu <hautonrandall@gmail.com>
2025-03-17 14:53:43 +0000

    please work

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) =>  {
     //sends the static file (login page) once server is run to port 8008
-    res.sendFile('login.html', {root: 'public'}, (err) => {
+    res.sendFile('contact.html', {root: 'public'}, (err) => {
         if(err) {
             console.log(err);
         }
