2e5477235fd50d532a529b98225a840cf71f73ba
Oliver Fawkes <64199725+fawkeso16@users.noreply.github.com>
2025-03-20 19:32:47 +0000

    Update achievement.js

--- app/public/achievement.js
+++ app/public/achievement.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const types = ["type1", "type2", "type3", "type4"];
 
     progressBars.forEach((bar, index) => {
-        let randomValue = Math.floor(Math.random() * 101); // 0-100% progress
-        let type = types[index % types.length]; // Cycle through 4 types
+        let randomValue = Math.floor(Math.random() * 101); 
+        let type = types[index % types.length]; 
 
-        bar.classList.remove("type1", "type2", "type3", "type4"); // Ensure no conflicts
-        bar.classList.add(type);  // Assign random color
-        bar.style.width = randomValue + "%"; // Random width
+        bar.classList.remove("type1", "type2", "type3", "type4"); 
+        bar.classList.add(type);  
+        bar.style.width = randomValue + "%"; h
     });
 });
