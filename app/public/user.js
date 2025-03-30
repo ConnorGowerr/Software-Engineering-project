5078a3016dbdf1398222148adc5a3a43c3757ac6
oliver fawkes <oliverfawkes@olivers-MacBook.local>
2025-03-30 20:06:52 +0100

    addeda few dynamic values to profile from a basic user mvc

---
+++ app/public/user.js
@@ -0,0 +1,45 @@
+class User {
+    constructor(userName, name, email, password, currentWeight, dob, height, gender) {
+        this.userName = userName;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.currentWeight = currentWeight;
+        this.dob = new Date(dob);
+        this.dateOfCreation = this.niceDate(new Date());
+        this.height = height;
+        this.gender = gender;
+        this.achievementPoints = 0;
+        this.allMeals = [];
+        this.allActivities = [];
+        this.allGroups = [];
+        this.allGoals = [];
+        this.allAchievements = [];
+    }
+
+    niceDate(date){
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+
+    getUserName() { return this.userName; }
+    getName() { return this.name; }
+    getEmail() { return this.email; }
+    getPassword() { return this.password; }
+    getCurrentWeight() { return this.currentWeight; }
+    getDob() { return this.dob; }
+    getDateOfCreation() { return this.dateOfCreation; }
+    getHeight() { return this.height; }
+    getGender() { return this.gender; }
+    getAchievementPoints() { return this.achievementPoints; }
+    getAllMeals() { return this.allMeals; }
+    getAllActivities() { return this.allActivities; }
+    getAllGroups() { return this.allGroups; }
+    getAllGoals() { return this.allGoals; }
+    getAllAchievements() { return this.allAchievements; }
+    
+    toString() {
+        return `User: ${this.userName}, Name: ${this.name}, Email: ${this.email}`;
+    }
+}
+
+export default User;
---
+++ app/public/userController.js
@@ -0,0 +1,65 @@
+import User from "./user.js"
+
+class UserController {
+    constructor() {
+        this.userInstance = new User(
+            'user123', 'John Doe', 'john@example.com', 'securepassword', 75.5,
+            '1995-05-20', 180, 'Male'
+        );
+    }
+
+    getUser() {
+        return this.userInstance;
+    }
+
+    calculateDailyCal(){
+        return 2000
+    }
+
+    //weight, height, gender, calories//
+
+    updateProfileStats(){
+
+        var loginInfo = document.querySelector(".mainArticleStats").children
+        let login1= loginInfo[0]
+        let login2 = loginInfo[1]
+        let login3 = loginInfo[2]
+
+        let created = login1.querySelector('br');
+        created.insertAdjacentHTML('afterend', `<p>${this.userInstance.dateOfCreation}</p>`);
+
+        // let br2 = stats2.querySelector('br');
+        // br2.insertAdjacentHTML('afterend', `<p>${this.userInstance.height}</p>`);
+
+        let lastLoggedIn = login3.querySelector('br');
+        lastLoggedIn.insertAdjacentHTML('afterend', `<p>${this.userInstance.dateOfCreation}</p>`);
+
+
+
+        var stats = document.querySelector(".mainstatsSection").children
+        let stats1 = stats[0]
+        let stats2 = stats[1]
+        let stats3 = stats[2]
+        let stats4 = stats[3]
+
+        let br1 = stats1.querySelector('br');
+        br1.insertAdjacentHTML('afterend', `<p>${this.userInstance.currentWeight}</p>`);
+
+        let br2 = stats2.querySelector('br');
+        br2.insertAdjacentHTML('afterend', `<p>${this.userInstance.height}</p>`);
+
+        let br3 = stats3.querySelector('br');
+        br3.insertAdjacentHTML('afterend', `<p>${this.userInstance.gender}</p>`);
+
+        let br4 = stats4.querySelector('br');
+        br4.insertAdjacentHTML('afterend', `<p>${this.calculateDailyCal()}</p>`);
+
+    
+
+    }
+}
+
+export default UserController;
+
+const userController = new UserController();
+userController.updateProfileStats();
