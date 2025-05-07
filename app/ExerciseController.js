cdd44bf64b3ac37ac500fea44668ada82df2b0cf
Harry-Auton-Randall <hautonrandall@gmail.com>
2025-05-07 16:47:34 +0100

    Inserting activity works, inserting user activity does not.

--- app/ExerciseController.js
+++ app/ExerciseController.js
@@ -1,5 +1,5 @@
 const dbClient = require('./public/db.js')
-
+const { randomInt } = require('crypto');
 
 class ExerciseController {
     constructor() {
@@ -49,45 +49,65 @@ class ExerciseController {
 
     //handles both activity and user activity
     saveActivity(req, res) {
-        const {logtime, activityid, username, name, duration, intensity} = req.body;
-    
+        const {username, exercisename, activityduration, activityintensity} = req.body;
+        let activityid = randomInt(1000000);
+        let idCount = 0;
     
         const insertQuery1 = `
-        INSERT INTO Activity (activityid, username, name, duration, intensity)
-        VALUES ($1, $2, $3, $4 $5)
+        INSERT INTO Activity (activityid, username, exercisename, duration, intensity)
+        VALUES ($1, $2, $3, $4, $5)
         `;
         const insertQuery2 = `
         INSERT INTO UserActivity (username, activityid, logtime)
-        VALUES ($1, $2, $3)
+        VALUES ($1, $2, CURRENT_TIMESTAMP)
         `;
+        const idCountQuery = `SELECT COUNT(*) FROM Exercise WHERE LOWER(activityid) = LOWER($1)`;
 
-        const values1 = [activityid, username, name, duration, intensity];
-        const values2 = [username, activityid, logtime];
+        const values1 = [activityid, username, exercisename, activityduration, activityintensity];
+        const values2 = [username, activityid];
 
         dbClient.query('SET SEARCH_PATH TO "Hellth", public;', (err) => {
             if (err) {
                 console.error("Search path error:", err);
                 return res.status(500).json({ error: "Failed to set search path" });
             }
-    
+
+            //check if the id is valid
+            // do{
+            //     activityid = randomInt(1000000);
+                
+            //     dbClient.query(idCountQuery, [activityid], (err, result1) => {
+            //         if (err) {
+            //             console.error("Database query error:", err);
+            //         }
+            //         return res.status(201).json({ message: "test", activity: result1 });
+            //     });
+            //     // idCount = idCountQuery.rows[0].count;
+            // }
+            // while(idCount > 0);
+            console.log(idCount)
+            console.log(activityid)
+
+
+
             //activity
-            dbClient.query(insertQuery1, values1, (err, result) => {
+            dbClient.query(insertQuery1, values1, (err, result2) => {
                 if (err) {
                     console.error( err);
                     return res.status(500).json({ error: "Failed to insert activity" });
                 }
     
-                return res.status(201).json({ message: "Activity inserted", activity: result.rows[0] });
+                return res.status(201).json({ message: "Activity inserted", activity: result2.rows[0] });
             });
             
             //user activity
-            dbClient.query(insertQuery2, values2, (err, result) => {
+            dbClient.query(insertQuery2, values2, (err, result3) => {
                 if (err) {
                     console.error( err);
                     return res.status(500).json({ error: "Failed to insert user activity" });
                 }
     
-                return res.status(201).json({ message: "User Activity inserted", useractivity: result.rows[0] });
+                return res.status(201).json({ message: "User Activity inserted", useractivity: result3.rows[0] });
             });
         });
     }
